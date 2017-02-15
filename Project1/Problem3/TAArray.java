1e81364f47cd2d9e44df68578df8cb691cc47c3f
Dana <dsa28@mail.aub.edu>
2017-02-15 21:28:31 +0200

    Constructors for TAArray - it can take integer values, double values or boolean values

--- Project1/Problem3/TAArray.java
+++ Project1/Problem3/TAArray.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
+
 class TAArray extends TAObject implements TAValue
 {
 
 	String type() {
-		return "a";
+		return "array";
 	}
 
 	
@@ -13,5 +15,50 @@ class TAArray extends TAObject implements TAValue
 	public void evaluate() {
 		
 	}
+	
+	TAArray(String a, int type, int size)
+	{
+		name = a;
+		this.size = size;
+		//arr = new TAArrayInt(size); 
+		
+	}
+	
+	<T extends TAInt>TAArray (String a, int type, T size)
+	{
+		this(a,type,size.value());
+	}
+	
+	TAArray(String a, double type, int size)
+	{
+		name = a;
+		this.size = size;
+		//arr = new TAArrayInt(size); 
+		
+	}
+	
+	<T extends TAInt>TAArray (String a,double type, T size)
+	{
+		this(a,type,size.value());
+	}
+	
+	TAArray(String a, boolean type, int size)
+	{
+		name = a;
+		this.size = size;
+		//arr = new TAArrayInt(size); 
+		
+	}
+	
+	<T extends TAInt>TAArray (String a, boolean type, T size)
+	{
+		this(a,type,size.value());
+	}
+	
+	
+	
+	
+	int size;
+	TAArray arr;
 	
 }
